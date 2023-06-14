31ebe59b3738be302d566860a9e73ee9a296596e
Daniel Wesley <97072132+weszzy@users.noreply.github.com>
2023-06-14 15:10:29 -0300

    Lots of changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function exportBugReports(fileFormat) {
     var projectName = document.getElementById('project-name').value;
     var hoursSpent = document.getElementById('hours-spent').value;
     var bugType = document.getElementById('bug-type').value;
-    var bugImportance = document.getElementById('bug-importance').value;
+    var bugPriority = document.getElementById('bug-priority').value;
     var bugNotes = document.getElementById('bug-notes').value;
 
     // Create the bug report object
@@ -12,85 +12,63 @@ function exportBugReports(fileFormat) {
         projectName: projectName,
         hoursSpent: hoursSpent,
         bugType: bugType,
-        bugImportance: bugImportance,
+        bugPriority: bugPriority,
         bugNotes: bugNotes
     };
 
     // Call the appropriate export function based on the selected file format
-    if (fileFormat === 'pdf') {
-        exportBugReportsAsPDF(bugReports);
-    } else if (fileFormat === 'txt') {
-        exportBugReportsAsTXT(bugReports);
+    if (fileFormat === 'txt') {
+        exportBugReportsAsTXT(bugReport);
     }
 }
 
-// Function to export bug reports as a PDF
-function exportBugReportsAsPDF(bugReports) {
-    // Define the document definition for pdfmake
-    var docDefinition = {
-        content: [
-            { text: 'Bug Reports', style: 'header' },
-            '\n',
-            { text: 'Project Name: ' + bugReports.projectName, style: 'subheader' },
-            '\n\n',
-            { text: 'Bug Reports:', style: 'subheader' },
-            '\n\n',
-        ]
-    };
-
-    // Add bug reports to the PDF document
-    bugReports.reports.forEach(function (report, index) {
-        docDefinition.content.push(
-            { text: 'Bug ' + (index + 1), style: 'bugHeader' },
-            'Project Name: ' + report.projectName,
-            'Hours Spent: ' + report.hoursSpent,
-            'Bug Type: ' + report.bugType,
-            'Bug Importance: ' + report.bugImportance,
-            'Bug Notes: ' + report.bugNotes,
-            '\n'
-        );
-    });
-
-    // Generate the PDF and initiate download
-    var pdfDocGenerator = pdfMake.createPdf(docDefinition);
-    pdfDocGenerator.download('bug_reports.pdf');
-}
-
 // Function to export bug reports as a TXT file
-function exportBugReportsAsTXT(bugReports) {
-    var textContent = 'Bug Reports\n\n';
-    textContent += 'Project Name: ' + bugReports.projectName + '\n\n';
-    textContent += 'Bug Reports:\n\n';
-
-    bugReports.reports.forEach(function (report, index) {
-        textContent += 'Bug ' + (index + 1) + '\n';
-        textContent += 'Project Name: ' + report.projectName + '\n';
-        textContent += 'Hours Spent: ' + report.hoursSpent + '\n';
-        textContent += 'Bug Type: ' + report.bugType + '\n';
-        textContent += 'Bug Importance: ' + report.bugImportance + '\n';
-        textContent += 'Bug Notes: ' + report.bugNotes + '\n\n';
-    });
+function exportBugReportsAsTXT(bugReport) {
+    var textContent = 'Bug Report\n\n';
+    textContent += 'Project Name: ' + bugReport.projectName + '\n';
+    textContent += 'Hours Spent: ' + bugReport.hoursSpent + '\n';
+    textContent += 'Bug Type: ' + bugReport.bugType + '\n';
+    textContent += 'Bug Priority: ' + bugReport.bugPriority + '\n';
+    textContent += 'Bug Notes: ' + bugReport.bugNotes + '\n\n';
 
     // Create a new Blob with the text content
     var blob = new Blob([textContent], { type: 'text/plain;charset=utf-8' });
 
     // Initiate download using FileSaver.js
-    saveAs(blob, 'bug_reports.txt');
+    saveAs(blob, 'bug_report.txt');
 }
-
-// Log the bug report
-console.log('Bug Report:', bugReport);
 
 // Reset the form
 document.getElementById('bug-report-form').reset();
-});
 
-var bugReports = {
-    projectName: projectName,
-    reports: [bugReport]
-};
+// Function to toggle the theme
+function toggleTheme() {
+    var body = document.body;
+    body.classList.toggle('dark-theme');
 
-// Call the export functions to generate and download the files
-exportBugReportsAsPDF(bugReports);
-exportBugReportsAsTXT(bugReports);
-});
+    // Save the user's theme preference
+    var themePreference = body.classList.contains('dark-theme') ? 'dark' : 'light';
+    localStorage.setItem('themePreference', themePreference);
+}
+
+// Function to detect the user's theme preference
+function detectUserTheme() {
+    var body = document.body;
+    var themePreference = localStorage.getItem('themePreference');
+
+    // Check if the user has a theme preference set
+    if (themePreference === 'dark') {
+        body.classList.add('dark-theme');
+    } else if (themePreference === 'light') {
+        body.classList.remove('dark-theme');
+    } else {
+        // If no preference is set, detect the user's system theme
+        var userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (userPrefersDark) {
+            body.classList.add('dark-theme');
+        }
+    }
+}
+
+// Call the detectUserTheme function when the page loads
+detectUserTheme();
